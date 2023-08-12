3a05ec5e99fc8c5cb5fbf080a9a76521526845db
shunsukekaneko72106 <61620829+shunsukekaneko72106@users.noreply.github.com>
2023-08-12 21:57:21 +0900

    サーバー起動まで

--- src/main/java/com/example/springtodoapp/app/task/TaskController.java
+++ src/main/java/com/example/springtodoapp/app/task/TaskController.java
@@ -1,5 +1,45 @@
 package com.example.springtodoapp.app.task;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.springtodoapp.service.TaskService;
+
+/**
+ * ToDoアプリコントローラー
+ */
+@Controller
+@RequestMapping("/task")
 public class TaskController {
   
+  //クラスを呼び出し
+  private final TaskService taskService;
+
+  @Autowired
+  public TaskController(TaskService taskService) {
+      this.taskService = taskService;
+  }
+
+    /**
+   * タスクの一覧を表示します
+   * @param taskForm
+   * @param model
+   * @return resources/templates下のHTMLファイル名
+   */
+  @GetMapping
+  public String task(Model model) {
+
+    //新規登録か更新かを判断する仕掛け
+
+    //Taskのリストを取得する
+    model.addAttribute("list", "");
+    model.addAttribute("title", "タスク一覧");
+
+    return "task/index";
+  }
+
+
 }
--- src/main/java/com/example/springtodoapp/service/TaskService.java
+++ src/main/java/com/example/springtodoapp/service/TaskService.java
@@ -1,22 +1,20 @@
-// package com.example.springtodoapp.service;
+package com.example.springtodoapp.service;
 
-// import java.util.List;
-// import java.util.Optional;
+import java.util.List;
+import java.util.Optional;
 
-// import com.example.demo.entity.Task;
+public interface TaskService {
 
-// public interface TaskService {
+	// List<Task> findAll();
 
-// 	List<Task> findAll();
+	// Optional<Task> getTask(int id);
 
-// 	Optional<Task> getTask(int id);
+	// void insert(Task task);
 
-// 	void insert(Task task);
+	// void update(Task task);
 
-// 	void update(Task task);
+	// void deleteById(int id);
 
-// 	void deleteById(int id);
+	// List<Task> findByType(int typeId);
 
-// 	List<Task> findByType(int typeId);
-
-// }
+}
