8672a6892d9f4d4144b9e83810314580e1d5c25c
shunsukekaneko72106 <61620829+shunsukekaneko72106@users.noreply.github.com>
2023-08-12 16:44:28 +0900

    Initial commit

---
+++ src/test/java/com/example/springtodoapp/SpringtodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
